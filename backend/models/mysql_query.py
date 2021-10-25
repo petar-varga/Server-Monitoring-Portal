492abd6cd177ae81439739a919f0134f30d92b9a
petar-varga <petarvarga128@gmail.com>
2021-10-25 20:56:14 +0200

    add mysql query model

--- backend/models/mysql_query.py
+++ backend/models/mysql_query.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, BigInteger, DateTime, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy.sql.schema import ForeignKey
 
 from db.base_class import Base
 
-class Server(Base):
-    __tablename__ = "server"
+class MySQLQuery(Base):
+    __tablename__ = "mysql_query"
 
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True, nullable=False)
     name = Column(Text, nullable=True, default=None)
@@ -14,6 +15,7 @@ class Server(Base):
     status = Column(Text, nullable=True, default="Initializing")
     date_added = Column(DateTime, nullable=True, default=func.now())
     date_updated = Column(DateTime, nullable=True, onupdate=func.now())
+    server_id = Column(BigInteger, ForeignKey("server.id"))
 
-    mysql_queries = relationship("MySQLQuery", back_populates="owner_server")
+    owner_server = relationship("Server", back_populates="mysql_queries")
     
